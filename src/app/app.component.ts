1c777f18356f057a54fa7dbe32934962c5ac64ce
Pops <pffijt@gmail.com>
2020-10-23 14:11:54 +0200

    improved css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,19 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = 'Pepijn Fijt | Portfolio | Software developer | Rotterdam';
 
-  showVideo(): boolean {
-    return window.screen.width > 1200;
-  }
+  showVideo = (): boolean => window.screen.width > 1200;
 
-  vh(v: number): number {
+  vh = (v: number): number => {
     const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return (v * h) / 100;
   }
 
-  vw(v: number): number {
+  vw = (v: number): number => {
     const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     return (v * w) / 100;
   }
 
-  changeNavigationStyle(addClass: string, removeClass?: string): void {
+  changeNavigationStyle = (addClass: string, removeClass?: string): void => {
     document.querySelectorAll('.navigation-bar-element').forEach(navElement => {
       navElement.classList.add(addClass);
       if (removeClass) {
@@ -32,7 +30,7 @@ export class AppComponent {
   }
 
   @HostListener('window:scroll', ['$event'])
-  changeNavigationStyleEvent(event): void {
+  changeNavigationStyleEvent = (event): void => {
     if (window.pageYOffset > this.vh(76)) {
       this.changeNavigationStyle('text-dark', 'text-light');
       document.getElementById('navigation-bar').classList.add('bg-light');
